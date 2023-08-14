3ea2ef4ad62b6c33db7bc4aed48c37d19677a1c4
BDP5 <142118954+BDP5@users.noreply.github.com>
2023-08-14 12:23:47 -0300

    Subroutine program_.py

---
+++ program_.py
@@ -0,0 +1,45 @@
+elif log.startswith("N"):
+                # Data parameter from variable 'log' is shown above (14)
+                    # Next the program displays current step and data processing for this part may start right after (15)
+                    print("Origen 1 to be sanded to Site 2")
+                    # (15)
+                    #
+                    #
+                    """dba = open('db.log', 'wb')
+                    import pickle
+                    pickle.dump(log, dba)
+                    dba.close()
+                    """
+                    #
+                    L = 'Origen 1 to be sanded to Site 2'
+                    #
+                    """#
+                    #
+                    # Data processing subroutine is implemented here (16)
+                    #
+                    # Code for data processing mast be placed here
+                    #
+                    #
+                    #
+                    """
+                    dbn = open('dbn2.log', 'w')
+                    dbn.write(L + '\n')
+                    dbn.close()
+                    # Parameter for first data prompt stage confirmation is set next (17)
+                    b = 1
+                    d = 1
+                    # Above is the parameter for current data processing confirmation and sequencing (17)
+                    #
+                    # Display from this segment confirmation is generated below (18)
+                    print("  ")
+                    # Above is the confirmation print from this segment (18)
+                    #
+                    #
+                    # Conclusion of this segment follows
+                    # Continuation of classification follows from this part of data segmentation to the next one(19)
+                    continue
+                    # Above is the step for next part (19)
+                    #
+                # Classification for data parameter is next (20)
+                else:
+                # (20)
